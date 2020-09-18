54a58009d2bfb9da40edb9f7bd8e87b32185ee04
aghayeva <sadaaghayeva@gmail.com>
2020-09-18 09:51:04 -0500

    update

--- src/test/java/com/cybertek/runners/RegressionRunner.java
+++ src/test/java/com/cybertek/runners/RegressionRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"html:target/cucumber-report.html", "json:target/cucumber.json", "rerun:target/rerun.txt"},
+        plugin = {"html:target/default-cucumber-reports", "json:target/cucumber.json", "rerun:target/rerun.txt"},
 
         features = "src/test/resources/features",
         glue = "com/cybertek/step_definitions",
